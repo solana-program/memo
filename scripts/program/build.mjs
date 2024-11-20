2a889a90243c39654d998cf96d5ff502888f45b4
Jon C <me@jonc.dev>
2024-11-20 01:00:34 +0100

    Add program to build

---
+++ scripts/program/build.mjs
@@ -0,0 +1,22 @@
+#!/usr/bin/env zx
+import 'zx/globals';
+import {
+  cliArguments,
+  getProgramFolders,
+  workingDirectory,
+} from '../utils.mjs';
+
+// Save external programs binaries to the output directory.
+import './dump.mjs';
+
+// Configure arguments here.
+const buildArgs = [
+  ...cliArguments()
+];
+
+// Build the programs.
+for (const folder of getProgramFolders()) {
+  const manifestPath = path.join(workingDirectory, folder, 'Cargo.toml');
+
+  await $`cargo-build-sbf --manifest-path ${manifestPath} ${buildArgs}`;
+}
