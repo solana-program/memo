596ed64461f5be717e7db3cdf8b3d0b71fdc0924
febo <febo@anza.xyz>
2025-06-08 16:40:35 +0100

    Add p-memo program

---
+++ p-memo/README.md
@@ -0,0 +1,54 @@
+# `p-memo`
+
+A `pinocchio`-based Memo program.
+
+## Overview
+
+`p-memo` is a reimplementation of the SPL Memo program using [`pinocchio`](https://github.com/anza-xyz/pinocchio). The program uses at most `~5%` of the compute units used by the current Memo program when signers are present; even when there are no signers, it needs only `~20%` of the current Memo program compute units. This efficiency is achieved by a combination of:
+1. `pinocchio` "lazy" entrypoint
+2. `sol_log_pubkey` syscall to log pubkey values
+3. [`pinocchio-log`](https://crates.io/crates/pinocchio-log) to format the memo message
+
+Since it uses the syscall to log pubkeys, the output of the program is slightly different while loging the same information:
+```
+1: Program PMemo11111111111111111111111111111111111111 invoke [1]
+2: Program log: Signed by:
+3: Program log: 1111111QLbz7JHiBTspS962RLKV8GndWFwiEaqKM
+4: Program log: Memo (len 60): "why does spl memo use 36000 cus to print len 60 msg of ascii"
+5: Program PMemo11111111111111111111111111111111111111 consumed 641 of 1400000 compute units
+6: Program PMemo11111111111111111111111111111111111111 success
+```
+
+Logging begins with entry into the program (`line 1`). Then there is a separate log to start the signers section (`line 2`); this is only present if there are signer accounts. After that there will be one line for each signer account (`line 3`), followed by the memo length and UTF-8 text (`line 4`). The program ends with the status of the instruction (`lines 5-6`).
+
+## Performance
+
+CU comsumption:
+
+| \# signers | p-memo      | SPL Memo  |
+| ---------- | ----------- | --------- |
+| 0          | 406 (`20%`) | 2,022     |
+| 1          | 632 (`5%`)  | 13,525    |
+| 2          | 749 (`3%`)  | 25,111    |
+| 3          | 866 (`2%`)  | 36,406    |
+
+> [!NOTE]
+> Using Solana CLI `v2.2.15`.
+
+## Building
+
+To build the program from its directory:
+```bash
+cargo build-sbf
+```
+
+## Testing
+
+To run the tests (after building the program):
+```bash
+SBF_OUT_DIR=../target/deploy cargo test
+```
+
+## License
+
+The code is licensed under the [Apache License Version 2.0](LICENSE)
---
+++ p-memo/tests/memo.rs
@@ -0,0 +1,122 @@
+use mollusk_svm::{result::Check, Mollusk};
+use solana_account::Account;
+use solana_instruction::{AccountMeta, Instruction};
+use solana_program_error::ProgramError;
+use solana_pubkey::Pubkey;
+
+/// Program ID for the p-memo program.
+const PROGRAM_ID: Pubkey = Pubkey::from_str_const("PMemo11111111111111111111111111111111111111");
+
+/// The memo to be printed.
+const MEMO: &str = "why does spl memo use 36000 cus to print len 60 msg of ascii";
+
+/// Creates an instruction for the p-memo program.
+fn instruction(message: &[u8], signers: Option<&[Pubkey]>) -> Instruction {
+    let accounts = if let Some(signers) = signers {
+        let mut accounts = Vec::with_capacity(signers.len());
+        for signer in signers {
+            accounts.push(AccountMeta::new_readonly(*signer, true));
+        }
+        accounts
+    } else {
+        Vec::new()
+    };
+
+    Instruction {
+        program_id: PROGRAM_ID,
+        accounts,
+        data: message.to_vec(),
+    }
+}
+
+#[test]
+fn test_valid_ascii_no_accounts() {
+    let mollusk = Mollusk::new(&PROGRAM_ID, "p_memo");
+
+    let instruction = instruction(MEMO.as_bytes(), None);
+
+    mollusk.process_and_validate_instruction(&instruction, &[], &[Check::success()]);
+}
+
+#[test]
+fn fail_test_invalid_ascii_no_accounts() {
+    let mollusk = Mollusk::new(&PROGRAM_ID, "p_memo");
+
+    let instruction = instruction(&[255, 255], None);
+
+    mollusk.process_and_validate_instruction(
+        &instruction,
+        &[],
+        &[Check::err(ProgramError::InvalidInstructionData)],
+    );
+}
+
+#[test]
+fn test_valid_ascii_one_accounts() {
+    let mollusk = Mollusk::new(&PROGRAM_ID, "p_memo");
+
+    let signer = Pubkey::new_unique();
+    let instruction = instruction(MEMO.as_bytes(), Some(&[signer]));
+
+    mollusk.process_and_validate_instruction(
+        &instruction,
+        &[(signer, Account::default())],
+        &[Check::success()],
+    );
+}
+
+#[test]
+fn test_valid_ascii_two_accounts() {
+    let mollusk = Mollusk::new(&PROGRAM_ID, "p_memo");
+
+    let signers = [Pubkey::new_unique(), Pubkey::new_unique()];
+    let instruction = instruction(MEMO.as_bytes(), Some(&signers));
+
+    mollusk.process_and_validate_instruction(
+        &instruction,
+        &signers
+            .iter()
+            .map(|signer| (*signer, Account::default()))
+            .collect::<Vec<(Pubkey, Account)>>(),
+        &[Check::success()],
+    );
+}
+
+#[test]
+fn test_valid_ascii_three_accounts() {
+    let mollusk = Mollusk::new(&PROGRAM_ID, "p_memo");
+
+    let signers = [
+        Pubkey::new_unique(),
+        Pubkey::new_unique(),
+        Pubkey::new_unique(),
+    ];
+    let instruction = instruction(MEMO.as_bytes(), Some(&signers));
+
+    mollusk.process_and_validate_instruction(
+        &instruction,
+        &signers
+            .iter()
+            .map(|signer| (*signer, Account::default()))
+            .collect::<Vec<(Pubkey, Account)>>(),
+        &[Check::success()],
+    );
+}
+
+#[test]
+fn test_valid_ascii_duplicated_accounts() {
+    let mollusk = Mollusk::new(&PROGRAM_ID, "p_memo");
+
+    let unique = Pubkey::new_unique();
+    let duplicated = Pubkey::new_unique();
+    let instruction = instruction(MEMO.as_bytes(), Some(&[duplicated, unique, duplicated]));
+
+    mollusk.process_and_validate_instruction(
+        &instruction,
+        &[
+            (duplicated, Account::default()),
+            (unique, Account::default()),
+        ],
+        &[Check::success()],
+    );
+}
