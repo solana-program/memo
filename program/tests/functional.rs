86e1feb1e677f4ed1c652ac3f0800435952d2790
Jon C <me@jonc.dev>
2025-11-24 20:33:38 +0100

    program: Port tests to mollusk, bump deps
    #### Problem
    
    The repo is still using solana-program-test and no v4 crates.
    
    #### Summary of changes
    
    Port tests to mollusk, and use the newer crates.

--- program/tests/functional.rs
+++ program/tests/functional.rs
@@ -1,124 +1,86 @@
 use {
-    solana_instruction::{error::InstructionError, AccountMeta, Instruction},
-    solana_program_test::*,
+    mollusk_svm::{result::Check, Mollusk},
+    solana_account::Account,
+    solana_instruction::{AccountMeta, Instruction},
+    solana_program_error::ProgramError,
     solana_pubkey::Pubkey,
-    solana_sdk::{
-        signature::{Keypair, Signer},
-        transaction::{Transaction, TransactionError},
-    },
     spl_memo::*,
 };
 
-fn program_test() -> ProgramTest {
-    ProgramTest::new("spl_memo", id(), processor!(processor::process_instruction))
-}
-
-#[tokio::test]
-async fn test_memo_signing() {
+#[test]
+fn test_memo_signing() {
     let memo = "🐆".as_bytes();
-    let (banks_client, payer, recent_blockhash) = program_test().start().await;
+    let mollusk = Mollusk::new(&id(), "spl_memo");
 
-    let keypairs = vec![Keypair::new(), Keypair::new(), Keypair::new()];
-    let pubkeys: Vec<Pubkey> = keypairs.iter().map(|keypair| keypair.pubkey()).collect();
+    let first_address = Pubkey::new_unique();
+    let second_address = Pubkey::new_unique();
+    let third_address = Pubkey::new_unique();
+    let pubkeys = [first_address, second_address, third_address];
 
     // Test complete signing
     let signer_key_refs: Vec<&Pubkey> = pubkeys.iter().collect();
-    let mut transaction =
-        Transaction::new_with_payer(&[build_memo(memo, &signer_key_refs)], Some(&payer.pubkey()));
-    let mut signers = vec![&payer];
-    for keypair in keypairs.iter() {
-        signers.push(keypair);
-    }
-    transaction.sign(&signers, recent_blockhash);
-    banks_client.process_transaction(transaction).await.unwrap();
+    mollusk.process_and_validate_instruction(
+        &build_memo(memo, &signer_key_refs),
+        &[
+            (first_address, Account::default()),
+            (second_address, Account::default()),
+            (third_address, Account::default()),
+        ],
+        &[Check::success()],
+    );
 
     // Test unsigned memo
-    let mut transaction =
-        Transaction::new_with_payer(&[build_memo(memo, &[])], Some(&payer.pubkey()));
-    transaction.sign(&[&payer], recent_blockhash);
-    banks_client.process_transaction(transaction).await.unwrap();
-
-    // Demonstrate success on signature provided, regardless of specific memo
-    // AccountMeta
-    let mut transaction = Transaction::new_with_payer(
-        &[Instruction {
-            program_id: id(),
-            accounts: vec![
-                AccountMeta::new_readonly(keypairs[0].pubkey(), true),
-                AccountMeta::new_readonly(keypairs[1].pubkey(), true),
-                AccountMeta::new_readonly(payer.pubkey(), false),
-            ],
-            data: memo.to_vec(),
-        }],
-        Some(&payer.pubkey()),
-    );
-    transaction.sign(&[&payer, &keypairs[0], &keypairs[1]], recent_blockhash);
-    banks_client.process_transaction(transaction).await.unwrap();
+    mollusk.process_and_validate_instruction(&build_memo(memo, &[]), &[], &[Check::success()]);
 
     // Test missing signer(s)
-    let mut transaction = Transaction::new_with_payer(
-        &[Instruction {
+    mollusk.process_and_validate_instruction(
+        &Instruction {
             program_id: id(),
             accounts: vec![
-                AccountMeta::new_readonly(keypairs[0].pubkey(), true),
-                AccountMeta::new_readonly(keypairs[1].pubkey(), false),
-                AccountMeta::new_readonly(keypairs[2].pubkey(), true),
+                AccountMeta::new_readonly(first_address, true),
+                AccountMeta::new_readonly(second_address, false),
+                AccountMeta::new_readonly(third_address, true),
             ],
             data: memo.to_vec(),
-        }],
-        Some(&payer.pubkey()),
-    );
-    transaction.sign(&[&payer, &keypairs[0], &keypairs[2]], recent_blockhash);
-    assert_eq!(
-        banks_client
-            .process_transaction(transaction)
-            .await
-            .unwrap_err()
-            .unwrap(),
-        TransactionError::InstructionError(0, InstructionError::MissingRequiredSignature)
+        },
+        &[
+            (first_address, Account::default()),
+            (second_address, Account::default()),
+            (third_address, Account::default()),
+        ],
+        &[Check::err(ProgramError::MissingRequiredSignature)],
     );
 
-    let mut transaction = Transaction::new_with_payer(
-        &[Instruction {
+    mollusk.process_and_validate_instruction(
+        &Instruction {
             program_id: id(),
             accounts: vec![
-                AccountMeta::new_readonly(keypairs[0].pubkey(), false),
-                AccountMeta::new_readonly(keypairs[1].pubkey(), false),
-                AccountMeta::new_readonly(keypairs[2].pubkey(), false),
+                AccountMeta::new_readonly(first_address, false),
+                AccountMeta::new_readonly(second_address, false),
+                AccountMeta::new_readonly(third_address, false),
             ],
             data: memo.to_vec(),
-        }],
-        Some(&payer.pubkey()),
-    );
-    transaction.sign(&[&payer], recent_blockhash);
-    assert_eq!(
-        banks_client
-            .process_transaction(transaction)
-            .await
-            .unwrap_err()
-            .unwrap(),
-        TransactionError::InstructionError(0, InstructionError::MissingRequiredSignature)
+        },
+        &[
+            (first_address, Account::default()),
+            (second_address, Account::default()),
+            (third_address, Account::default()),
+        ],
+        &[Check::err(ProgramError::MissingRequiredSignature)],
     );
 
     // Test invalid utf-8; demonstrate log
     let invalid_utf8 = [0xF0, 0x9F, 0x90, 0x86, 0xF0, 0x9F, 0xFF, 0x86];
-    let mut transaction =
-        Transaction::new_with_payer(&[build_memo(&invalid_utf8, &[])], Some(&payer.pubkey()));
-    transaction.sign(&[&payer], recent_blockhash);
-    assert_eq!(
-        banks_client
-            .process_transaction(transaction)
-            .await
-            .unwrap_err()
-            .unwrap(),
-        TransactionError::InstructionError(0, InstructionError::InvalidInstructionData)
+    mollusk.process_and_validate_instruction(
+        &build_memo(&invalid_utf8, &[]),
+        &[],
+        &[Check::err(ProgramError::InvalidInstructionData)],
     );
 }
 
-#[tokio::test]
-#[ignore]
-async fn test_memo_compute_limits() {
-    let (banks_client, payer, recent_blockhash) = program_test().start().await;
+#[test]
+fn test_memo_compute_limits() {
+    let mollusk = Mollusk::new(&id(), "spl_memo");
 
     // Test memo length
     let mut memo = vec![];
@@ -127,24 +89,17 @@ async fn test_memo_compute_limits() {
         memo.append(&mut vec);
     }
 
-    let mut transaction =
-        Transaction::new_with_payer(&[build_memo(&memo[..450], &[])], Some(&payer.pubkey()));
-    transaction.sign(&[&payer], recent_blockhash);
-    banks_client.process_transaction(transaction).await.unwrap();
+    mollusk.process_and_validate_instruction(
+        &build_memo(&memo[..450], &[]),
+        &[],
+        &[Check::success()],
+    );
 
-    let mut transaction =
-        Transaction::new_with_payer(&[build_memo(&memo[..600], &[])], Some(&payer.pubkey()));
-    transaction.sign(&[&payer], recent_blockhash);
-    let err = banks_client
-        .process_transaction(transaction)
-        .await
-        .unwrap_err()
-        .unwrap();
-    let failed_to_complete =
-        TransactionError::InstructionError(0, InstructionError::ProgramFailedToComplete);
-    let computational_budget_exceeded =
-        TransactionError::InstructionError(0, InstructionError::ComputationalBudgetExceeded);
-    assert!(err == failed_to_complete || err == computational_budget_exceeded);
+    mollusk.process_and_validate_instruction(
+        &build_memo(&memo[..600], &[]),
+        &[],
+        &[Check::success(), Check::compute_units(8_674)],
+    );
 
     let mut memo = vec![];
     for _ in 0..100 {
@@ -152,54 +107,45 @@ async fn test_memo_compute_limits() {
         memo.append(&mut vec);
     }
 
-    let mut transaction =
-        Transaction::new_with_payer(&[build_memo(&memo[..60], &[])], Some(&payer.pubkey()));
-    transaction.sign(&[&payer], recent_blockhash);
-    banks_client.process_transaction(transaction).await.unwrap();
+    mollusk.process_and_validate_instruction(
+        &build_memo(&memo[..60], &[]),
+        &[],
+        &[Check::success()],
+    );
 
-    let mut transaction =
-        Transaction::new_with_payer(&[build_memo(&memo[..63], &[])], Some(&payer.pubkey()));
-    transaction.sign(&[&payer], recent_blockhash);
-    let err = banks_client
-        .process_transaction(transaction)
-        .await
-        .unwrap_err()
-        .unwrap();
-    assert!(err == failed_to_complete || err == computational_budget_exceeded);
+    mollusk.process_and_validate_instruction(
+        &build_memo(&memo[..63], &[]),
+        &[],
+        &[Check::success(), Check::compute_units(67_468)],
+    );
 
     // Test num signers with 32-byte memo
-    let memo = Pubkey::new_unique().to_bytes();
-    let mut keypairs = vec![];
+    let memo = [b'1'; 32];
+    let mut pubkeys = vec![];
     for _ in 0..20 {
-        keypairs.push(Keypair::new());
+        pubkeys.push(Pubkey::new_unique());
     }
-    let pubkeys: Vec<Pubkey> = keypairs.iter().map(|keypair| keypair.pubkey()).collect();
     let signer_key_refs: Vec<&Pubkey> = pubkeys.iter().collect();
 
-    let mut signers = vec![&payer];
-    for keypair in keypairs[..12].iter() {
-        signers.push(keypair);
-    }
-    let mut transaction = Transaction::new_with_payer(
-        &[build_memo(&memo, &signer_key_refs[..12])],
-        Some(&payer.pubkey()),
+    mollusk.process_and_validate_instruction(
+        &build_memo(&memo, &signer_key_refs[..12]),
+        pubkeys
+            .iter()
+            .take(12)
+            .map(|k| (*k, Account::default()))
+            .collect::<Vec<_>>()
+            .as_slice(),
+        &[Check::success()],
     );
-    transaction.sign(&signers, recent_blockhash);
-    banks_client.process_transaction(transaction).await.unwrap();
 
-    let mut signers = vec![&payer];
-    for keypair in keypairs[..15].iter() {
-        signers.push(keypair);
-    }
-    let mut transaction = Transaction::new_with_payer(
-        &[build_memo(&memo, &signer_key_refs[..15])],
-        Some(&payer.pubkey()),
+    mollusk.process_and_validate_instruction(
+        &build_memo(&memo, &signer_key_refs[..15]),
+        pubkeys
+            .iter()
+            .take(15)
+            .map(|k| (*k, Account::default()))
+            .collect::<Vec<_>>()
+            .as_slice(),
+        &[Check::success(), Check::compute_units(30_900)],
     );
-    transaction.sign(&signers, recent_blockhash);
-    let err = banks_client
-        .process_transaction(transaction)
-        .await
-        .unwrap_err()
-        .unwrap();
-    assert!(err == failed_to_complete || err == computational_budget_exceeded);
 }
